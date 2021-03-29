81bbfc3b3f590c42b5bc3bd59c2313311dd0f4e3
lilylwang <lily.l.wang@gmail.com>
2021-03-29 12:25:08 -0400

    Update store
    Update images, basic cart

--- store.js
+++ store.js
@@ -1,34 +1,59 @@
-$('.like-btn').on('click', function() {
-    $(this).toggleClass('is-active');
-});
+var cart = [];
 
-$('.minus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value &amp;amp;gt; 1) {
-        value = value - 1;
-    } else {
-        value = 0;
-    }
- 
-  $input.val(value);
- 
-});
- 
-$('.plus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value &amp;amp;lt; 100) {
-        value = value + 1;
-    } else {
-        value =100;
-    }
- 
-    $input.val(value);
-});
+function display(){
+    var stringCart = cart.toString();
+    document.getElementById('cart').innerHTML=stringCart; 
+}
+
+function tempFunction(){
+    cart.push("Thermometer");
+    window.confirm("Add a thermometer to cart");
+    display();
+}
+
+function maskFunction(){
+    cart.push("Face Masks");
+    window.confirm("Add face mask to cart");
+    display();
+}
+
+function vitCFunction(){
+    cart.push("Vitamin C");
+    window.confirm("Add vitamin C to cart");
+    display();
+}
+
+function motrinFunction(){
+    cart.push("Motrin");
+    window.confirm("Add motrin to cart");
+    display();
+}
+
+function claritinFunction(){
+    cart.push("Claritin");
+    window.confirm("Add claritin to cart");
+    display();
+}
+function coldEezeFunction(){
+    cart.push("Cold Eeze");
+    window.confirm("Add Cold Eeze to cart");
+    display();
+}
+
+function wipesFunction(){
+    cart.push("Wipes");
+    window.confirm("Add wipes to cart");
+    display();
+}
+
+function EmergenCFunction(){
+    cart.push("EmergenC");
+    window.confirm("Add EmergenC to cart");
+    display();
+}
+
+function soapFunction(){
+    cart.push("Soap");
+    window.confirm("Add soap to cart");
+    display();
+}
