bf529c6dffcab8384522e60a907945ccd6ff03c8
lilylwang <lily.l.wang@gmail.com>
2021-03-29 12:53:35 -0400

    Update store
    Fixed window alert, add total cost

--- store.js
+++ store.js
@@ -1,59 +1,83 @@
 var cart = [];
+var cost = 0;
+
 
 function display(){
     var stringCart = cart.toString();
-    document.getElementById('cart').innerHTML=stringCart; 
+    var roundedCost = cost.toFixed(2)
+    //remember to round
+    var costString = roundedCost.toString();
+    document.getElementById('cart').innerHTML=stringCart;
+    document.getElementById('cost').innerHTML="Total Cost: $" + costString;
 }
 
 function tempFunction(){
-    cart.push("Thermometer");
-    window.confirm("Add a thermometer to cart");
+    if(window.confirm("Add a thermometer to cart")){
+        cart.push("Thermometer");
+        cost += 19.99;
+    };
     display();
 }
 
 function maskFunction(){
-    cart.push("Face Masks");
-    window.confirm("Add face mask to cart");
+    if(window.confirm("Add face mask to cart")){
+        cart.push("Face Masks");
+        cost += 24.99;
+    }
     display();
 }
 
 function vitCFunction(){
-    cart.push("Vitamin C");
-    window.confirm("Add vitamin C to cart");
+    if(window.confirm("Add vitamin C to cart")){
+        cart.push("Vitamin C");
+        cost += 5.99
+    }
     display();
 }
 
 function motrinFunction(){
-    cart.push("Motrin");
-    window.confirm("Add motrin to cart");
+    if(window.confirm("Add motrin to cart")){
+        cart.push("Motrin");
+        cost +=5.99;
+    }
     display();
 }
 
 function claritinFunction(){
-    cart.push("Claritin");
-    window.confirm("Add claritin to cart");
+    if(window.confirm("Add claritin to cart")){
+        cart.push("Claritin");
+        cost += 9.99;
+    }
     display();
 }
-function coldEezeFunction(){
-    cart.push("Cold Eeze");
-    window.confirm("Add Cold Eeze to cart");
+function coldEezeFunction(){ 
+    if(window.confirm("Add Cold Eeze to cart")){
+        cart.push("Cold Eeze");
+        cost += 6.99;
+    }
     display();
 }
 
 function wipesFunction(){
-    cart.push("Wipes");
-    window.confirm("Add wipes to cart");
+    if(window.confirm("Add wipes to cart")){
+        cart.push("Wipes");
+        cost += 7.99;
+    }
     display();
 }
 
-function EmergenCFunction(){
-    cart.push("EmergenC");
-    window.confirm("Add EmergenC to cart");
+function EmergenCFunction(){ 
+    if(window.confirm("Add EmergenC to cart")){
+        cart.push("EmergenC");
+        cost += 9.99;
+    }
     display();
 }
 
 function soapFunction(){
-    cart.push("Soap");
-    window.confirm("Add soap to cart");
+    if(window.confirm("Add soap to cart")){
+        cart.push("Soap");
+        cost += 3.99;
+    }
     display();
 }
